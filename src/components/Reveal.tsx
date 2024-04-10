1de9c2a7090268aaa9a53e58d1b7281534c9d199
giorgio324 <giorgikochuashvili2002@gmail.com>
2024-04-10 22:28:18 +0400

    initilized next + typescript project installed framer motion added boilerplate code

---
+++ src/components/Reveal.tsx
@@ -0,0 +1,50 @@
+'use client';
+import { motion, useAnimation, useInView } from 'framer-motion';
+import { useEffect, useRef } from 'react';
+
+type Props = {
+  children: React.ReactNode;
+  width?: 'fit-content' | '100%';
+};
+
+const Reveal = ({ children, width }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+  useEffect(() => {
+    console.log(isInView);
+    if (isInView) {
+      console.log('i am visible');
+      mainControls.start('visible');
+      slideControls.start('visible');
+    }
+  }, [isInView]);
+  return (
+    <div ref={ref} style={{ position: 'relative', width, overflow: 'hidden' }}>
+      <motion.div
+        variants={{
+          start: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial='start'
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          start: { left: 0 },
+          visible: { left: '100%' },
+        }}
+        initial={'hidden'}
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: 'easeIn' }}
+        className='absolute top-4 left-0 bottom-4 right-0 bg-green-500 z-20'
+      />
+    </div>
+  );
+};
+export default Reveal;
