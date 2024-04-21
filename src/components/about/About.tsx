af24eab9b100533eb1847eae296bb942963ef5ea
giorgio324 <giorgikochuashvili2002@gmail.com>
2024-04-21 21:55:16 +0400

    about me image swapped and finished it for mobile

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -3,7 +3,7 @@ import AboutTitle from './AboutTitle';
 
 const About = () => {
   return (
-    <div className='max-w-screen-xl mx-auto mt-16 md:mt-[96px] px-4 md:px-10 lg:px-[108px] py-10 md:pt-[140px] md:pb-[160px] md:flex justify-center items-center gap-8'>
+    <div className='max-w-screen-xl mx-auto px-4 md:px-10 lg:px-[108px] py-10 md:pt-[140px] md:pb-[160px]'>
       <AboutTitle />
       <AboutImages />
     </div>
--- src/components/about/AboutTitle.tsx
+++ src/components/about/AboutTitle.tsx
@@ -1,28 +1,32 @@
 import RevealWithBackground from '../animation/RevealWithBackground';
 const AboutTitle = () => {
   return (
-    <section>
-      <RevealWithBackground>
-        <h1 className='text-4xl lg:text-5xl flex flex-col bebas-neue'>
-          About me
-        </h1>
-      </RevealWithBackground>
-      <div className='mt-4'>
+    <section className='md:flex justify-between gap-10'>
+      <div>
         <RevealWithBackground>
-          <p className='text-xl manrope font-medium'>
-            👋 I am a Student who is passionate about web development
-          </p>
+          <h1 className='text-4xl lg:text-5xl flex flex-col bebas-neue w-max'>
+            About me
+          </h1>
         </RevealWithBackground>
       </div>
-      <div className='mt-2'>
-        <RevealWithBackground>
-          <p className='text-paragraphTextColor text-lg manrope'>
-            I discovered my passion for web development when I started
-            university, where I primarily delve into front-end development using
-            React and Next.js. However, I'm also eager to explore technologies
-            outside of front-end.
-          </p>
-        </RevealWithBackground>
+      <div className='md:max-w-lg'>
+        <div className='mt-4 md:mt-0'>
+          <RevealWithBackground>
+            <p className='text-xl manrope font-medium'>
+              👋 I am a Student who is passionate about web development
+            </p>
+          </RevealWithBackground>
+        </div>
+        <div className='mt-2'>
+          <RevealWithBackground>
+            <p className='text-paragraphTextColor text-lg manrope'>
+              I discovered my passion for web development when I started
+              university, where I primarily delve into front-end development
+              using React and Next.js. However, I'm also eager to explore
+              technologies outside of front-end.
+            </p>
+          </RevealWithBackground>
+        </div>
       </div>
     </section>
   );
